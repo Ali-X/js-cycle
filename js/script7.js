4aae58a67c36b9dfd868a862506048eecc420d3f
Ali-X <alina240895@yandex.ru>
2018-12-12 22:28:19 +0200

    Implement seventh task

---
+++ js/script7.js
@@ -0,0 +1,13 @@
+let userString = prompt("Enter string");
+
+function reverseString() {
+    let output = "";
+
+    for (let i = userString.length - 1; i >= 0; i--) {
+        output += userString[i];
+    }
+
+    return output;
+}
+
+console.log("Reversed string for \"" + userString + "\" is \"" + reverseString() + "\"");
