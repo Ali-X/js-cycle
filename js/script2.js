b850f11f3cdb8bf2578ccd78065814b2a802bb03
Ali-X <alina240895@yandex.ru>
2018-12-12 21:33:17 +0200

    Implement second task

---
+++ js/script2.js
@@ -0,0 +1,21 @@
+let userNumber = Number(prompt("Enter natural number"));
+let resultArr = [];
+
+function printNaturalNumbers() {
+    outer: for (let i = 2; i <= userNumber; i++) {
+        for (let j = 2; j < i; j++) {
+            if (i % j === 0) {
+                continue outer;
+            }
+        }
+        resultArr.push(i);
+    }
+
+    console.log("Natural numbers: " + resultArr);
+}
+
+if (isNaN(userNumber)) {
+    console.log("Please enter numeric data");
+} else {
+    printNaturalNumbers();
+}
