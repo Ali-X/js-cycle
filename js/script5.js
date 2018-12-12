439c085cf5dc424a416a757de351b1301f058365
Ali-X <alina240895@yandex.ru>
2018-12-12 22:03:37 +0200

    Implement fifth task

---
+++ js/script5.js
@@ -0,0 +1,15 @@
+var minNum = Number(prompt("Enter min number"));
+var maxNum = Number(prompt("Enter max number"));
+
+if (isNaN(minNum) || isNaN(maxNum)) {
+    console.log("Enter numberic data");
+} else if (minNum >= maxNum) {
+    console.log("The min number should be less than max number");
+} else {
+    console.log(getRandomNumber())
+}
+
+function getRandomNumber() {
+    return Math.floor((Math.random() * (maxNum - minNum + 1)) + minNum);
+}
+
